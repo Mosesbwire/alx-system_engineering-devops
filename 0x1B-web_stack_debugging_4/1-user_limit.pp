74fa60f12cefc8b69735aa85ec97614ec22f504c
Mosesbwire <mosesdev92@gmail.com>
2023-09-03 09:55:19 +0000

    Complete puppet config files to fix errors

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,15 @@
+
+# and open a file without any error message.
+
+exec { 'correct-hard':
+  command  => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf',
+  provider => shell,
+}
+exec { 'correct-soft':
+  command  => 'sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
+  provider => shell,
+}
+exec { 'change-os-configuration-for-holberton-user':
+  command  => 'sysctl -p',
+  provider => shell,
+}
